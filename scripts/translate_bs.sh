73038144e11a1844ad8c6d779ca05b20b3ad64a3
Ernad Husremovic <hernad@bring.out.ba>
2020-10-21 19:07:18 +0200

    submodule custom/contract

--- scripts/translate_bs.sh
+++ scripts/translate_bs.sh
@@ -1,3 +1,3 @@
 #DELETE FROM ir_translation WHERE lang = 'bs_BA'
 
-python2 odoo-bin --db_host=127.0.0.1 -r odoo -w test01 --addons-path=addons -d odoo10 -l bs_BA --load-language bs_BA
+python2 odoo-bin --db_host=127.0.0.1 -r odoo -w test01 --addons-path=addons,custom/contract -d odoo10 -l bs_BA --load-language bs_BA
