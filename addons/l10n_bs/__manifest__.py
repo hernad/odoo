e12864aea3a11b13051cb7b7af99604368ba5690
Ernad Husremovic <hernad@bring.out.ba>
2020-10-24 17:58:30 +0200

    l10n_bs manifest

--- addons/l10n_bs/__manifest__.py
+++ addons/l10n_bs/__manifest__.py
@@ -1,53 +1,39 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-# Author: Goran Kliska
-# mail:   goran.kliska(AT)slobodni-programi.hr
-# Copyright (C) 2011- Slobodni programi d.o.o., Zagreb
-# Contributions:
-#           Tomislav Bošnjaković, Storm Computers d.o.o. :
-#              - account types
-
+# Author: Ernad Husremovic
+# mail:   hernad(AT)bring.out.ba
 {
-    "name": "Croatia - Accounting (RRIF 2012)",
+    "name": "Bosnia - Accounting (FBiH 2020)",
     "description": """
-Croatian localisation.
+Bosnian localisation.
 ======================
 
-Author: Goran Kliska, Slobodni programi d.o.o., Zagreb
-        https://www.slobodni-programi.hr
-
-Contributions:
-  Tomislav Bošnjaković, Storm Computers: tipovi konta
-  Ivan Vađić, Slobodni programi: tipovi konta
+Author: Ernad Husremovic bring.out doo Sarajevo
 
 Description:
 
-Croatian Chart of Accounts (RRIF ver.2012)
+Bosnian Chart of Accounts (FBiH ver.2020)
 
-RRIF-ov računski plan za poduzetnike za 2012.
+FBiH knjigovodstveni kontni plan za preduzeća za 2020.
 Vrste konta
-Kontni plan prema RRIF-u, dorađen u smislu kraćenja naziva i dodavanja analitika
 Porezne grupe prema poreznoj prijavi
 Porezi PDV obrasca
 Ostali porezi
 Osnovne fiskalne pozicije
 
-Izvori podataka:
- https://www.rrif.hr/dok/preuzimanje/rrif-rp2011.rar
- https://www.rrif.hr/dok/preuzimanje/rrif-rp2012.rar
 
 """,
-    "version": "13.0",
-    "author": "OpenERP Croatian Community",
+    "version": "10.0",
+    "author": "bring.out",
     'category': 'Localization',
-    "website": "https://code.launchpad.net/openobject-croatia",
+    "website": "https://github.com/hernad/odoo",
 
     'depends': [
         'account',
     ],
     'data': [
-        'data/l10n_hr_chart_data.xml',
+        'data/l10n_bs_chart_data.xml',
         'data/account.account.type.csv',
         'data/account.account.template.csv',
         'data/account_chart_tag_data.xml',
