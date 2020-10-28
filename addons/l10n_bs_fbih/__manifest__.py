976f29c6243e543e9867e83e15b40d72157b612d
Ernad Husremovic <hernad@bring.out.ba>
2020-10-28 19:04:42 +0100

    bs account.account - deprecated: True if jednocifren, dvocifren, trocifren

--- addons/l10n_bs_fbih/__manifest__.py
+++ addons/l10n_bs_fbih/__manifest__.py
@@ -15,20 +15,22 @@ Bosnian Chart of Accounts (FBiH ver.2020)
 
 
 """,
-    "version": "10.0",
+    "version": "10.0.3",
     "author": "bring.out",
     'category': 'Localization',
     "website": "https://github.com/hernad/odoo",
 
     'depends': [
-        'account',
+        'l10n_bs',
     ],
     'data': [
         'data/l10n_bs_fbih_chart_data.xml',
         'data/account.account.template.csv',
         'data/account_chart_tag_data.xml',
+        'data/account.tax.template.csv',
         'data/account_tax_fiscal_position_data.xml',
         'data/account_chart_template_data.yml',
+        'data/account_updates_data.xml'
     ],
     "active": False,
 }
