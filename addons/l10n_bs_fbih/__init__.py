9a83745dbaaaaed782fdb830a38a029c943a694b
Ernad Husremovic <hernad@bring.out.ba>
2020-10-28 14:56:13 +0100

    l10n_bs_fbih

---
+++ addons/l10n_bs_fbih/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Author: Ernad Husremovic
+# mail:   hernad@bring.out.ba
+# Copyright (C) 2020-2020 bring.out doo Sarajevo 
---
+++ addons/l10n_bs_fbih/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Author: Ernad Husremovic
+# mail:   hernad@bring.out.ba
+{
+    "name": "Bosna i Hercegovina / FBiH 2020",
+    "description": """BiH, kontni plan FBiH
+
+Author: Ernad Husremovic bring.out doo Sarajevo
+
+Description:
+
+Bosnian Chart of Accounts (FBiH ver.2020)
+
+
+""",
+    "version": "10.0",
+    "author": "bring.out",
+    'category': 'Localization',
+    "website": "https://github.com/hernad/odoo",
+
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'data/l10n_bs_chart_data.xml',
+        'data/account.account.template.csv',
+        'data/account_chart_tag_data.xml',
+        'data/account_tax_fiscal_position_data.xml',
+        'data/account_chart_template_data.yml',
+    ],
+    "active": False,
+}
