b4d33e1cb35e9226eeb69d265ca659b388de6aca
Ernad Husremovic <hernad@bring.out.ba>
2020-10-30 15:48:33 +0100

    bs translation updates

--- scripts/demo_de.sh
+++ scripts/demo_de.sh
@@ -1,6 +1,8 @@
 #DELETE FROM ir_translation WHERE lang = 'bs_BA'
 
-python2 odoo-bin --db_host=127.0.0.1 -r odoo -w test01 --addons-path=addons,custom/contract,custom/account-fiscal-rule -d odoo10de -l bs_BA --load-language bs_BA \
+python2 odoo-bin --db_host=127.0.0.1 -r odoo -w test01 \
+           --addons-path=addons,custom/contract,custom/account-fiscal-rule,custom/report,custom/muk_web \
+           -d odoo10de -l bs_BA --load-language bs_BA \
            --without-demo=all
 
 #https://www.odoo.com/forum/help-1/question/exception-bus-bus-unavailable-odoo-10-124387
