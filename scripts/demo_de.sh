e90a0474c824bc835a5ec260c27029403f4ed966
Ernad Husremovic <hernad@bring.out.ba>
2020-10-30 11:33:16 +0100

    računovodstvo - bosanski prevod

--- scripts/demo_de.sh
+++ scripts/demo_de.sh
@@ -1,4 +1,12 @@
 #DELETE FROM ir_translation WHERE lang = 'bs_BA'
 
-python2 odoo-bin --db_host=127.0.0.1 -r odoo -w test01 --addons-path=addons,custom/contract -d odoo10de -l bs_BA --load-language bs_BA \
-           --without-demo=all 
+python2 odoo-bin --db_host=127.0.0.1 -r odoo -w test01 --addons-path=addons,custom/contract,custom/account-fiscal-rule -d odoo10de -l bs_BA --load-language bs_BA \
+           --without-demo=all
+
+#https://www.odoo.com/forum/help-1/question/exception-bus-bus-unavailable-odoo-10-124387
+#	   -c odoo.conf
+
+#-u all 
+
+#https://www.odoo.com/documentation/10.0/setup/deploy.html
+
